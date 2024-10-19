5f839a919a756a7a1aa9b49b42ed5f8d311ae652
warren-luo <warrenluo@126.com>
2024-10-19 13:12:46 +0800

    first commit

---
+++ src/test/java/com/warrenluo/ssoclient/SsoclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warrenluo.ssoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
